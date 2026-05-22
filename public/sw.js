1951873d07bd8ff80cfb0264f92762de9ef5b253
Philippe Saxer <philippesaxer8@gmail.com>
2026-05-22 22:59:31 +0200

    Install Monetag Push Notifications

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11044246
+    "domain": "3nbf4.com",
+    "zoneId": 11044327
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
